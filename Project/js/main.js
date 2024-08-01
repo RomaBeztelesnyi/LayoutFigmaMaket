d295503fa2bf2d23ea8904a9a4c2e3b70f535d63
RomaBeztelesnyi <mavric1234qwer@gmail.com>
2024-08-01 20:27:26 +0300

    Dark-mode

--- Project/js/main.js
+++ Project/js/main.js
@@ -1,6 +1,25 @@
 const btnDarkMode = document.querySelector(".dark-mode-btn")
 
-btnDarkMode.onclick = function() {
-    btnDarkMode.classList.toggle("dark-mode-btn--active");
-    document.body.classList.toggle("dark")
+if (localStorage.getItem('darkMode') === 'dark') {
+    btnDarkMode.classList.add("dark-mode-btn--active");
+    document.body.classList.add("dark");
+} else if (localStorage.getItem('darkMode') === 'light');
+
+// Checking dark mode in OS 
+
+if(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark').matches ) {
+    btnDarkMode.classList.add("dark-mode-btn--active");
+    document.body.classList.add("dark");
 }
+
+//Turning on the night mode
+btnDarkMode.onclick = function () {
+    btnDarkMode.classList.toggle("dark-mode-btn--active");
+    const isDark = document.body.classList.toggle("dark");
+
+    if (isDark) {
+        localStorage.setItem("darkMode", "dark");
+    } else {
+        localStorage.setItem("darkMode", "light");
+    }
+};
