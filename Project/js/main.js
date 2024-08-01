7c508e3c3070cf47ce38189e453c395dcf2f81be
RomaBeztelesnyi <mavric1234qwer@gmail.com>
2024-08-01 20:50:52 +0300

    First

--- Project/js/main.js
+++ Project/js/main.js
@@ -1,9 +1,5 @@
 const btnDarkMode = document.querySelector(".dark-mode-btn")
 
-if (localStorage.getItem('darkMode') === 'dark') {
-    btnDarkMode.classList.add("dark-mode-btn--active");
-    document.body.classList.add("dark");
-} else if (localStorage.getItem('darkMode') === 'light');
 
 // Checking dark mode in OS 
 
@@ -11,6 +7,34 @@ if(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark').matches
     btnDarkMode.classList.add("dark-mode-btn--active");
     document.body.classList.add("dark");
 }
+
+// Priority dark Scheme
+
+if (localStorage.getItem('darkMode') === 'dark') {
+    btnDarkMode.classList.add("dark-mode-btn--active");
+    document.body.classList.add("dark");
+} else if (localStorage.getItem('darkMode') === 'light') {
+    btnDarkMode.classList.remove('dark-mode-btn--active');
+    document.body.classList.remove('dark');
+}
+
+
+// if change system setting, change scheme
+
+window
+    .matchMedia("(prefer-color-scheme:dark)")
+    .addEventListener('change', (event) => {
+        const newColorScheme = event.matches ? "dark" : "light";
+        if (newColorScheme === 'dark') {
+            btnDarkMode.classList.add('dark-mode-btn--active');
+            document.body.classList.add('dark');
+            localStorage.setItem("darkMode", "dark");
+        } else {
+            btnDarkMode.classList.remove('dark-mode-btn--active');
+            document.body.classList.remove('dark');
+            localStorage.setItem("darkMode", "light");
+        }
+})
 
 //Turning on the night mode
 btnDarkMode.onclick = function () {
