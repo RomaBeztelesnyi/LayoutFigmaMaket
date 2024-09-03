33477b779a1fe3e2da94efc76ec484baea4bdcad
RomaBeztelesnyi <mavric1234qwer@gmail.com>
2024-09-03 19:10:35 +0300

    fix-bugs

--- designo/js/main.js
+++ designo/js/main.js
@@ -1,7 +1,6 @@
-document.querySelector('.header__burger').addEventListener('click', function() {
+document.querySelector('.header__burger').addEventListener('click', () => {
     document.querySelector('.header').classList.toggle('open');
     // document.querySelector('.header_nav').classList.toggle('container')
-
 });
 
 
