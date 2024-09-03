80af0abbd029839ee06ad856b78930569a3fa914
RomaBeztelesnyi <mavric1234qwer@gmail.com>
2024-09-03 18:58:49 +0300

    Finished-burger-menu

--- designo/js/main.js
+++ designo/js/main.js
@@ -1,3 +1,7 @@
 document.querySelector('.header__burger').addEventListener('click', function() {
-    document.querySelector('.header').classList.toggle('open')
+    document.querySelector('.header').classList.toggle('open');
+    // document.querySelector('.header_nav').classList.toggle('container')
+
 });
+
+
