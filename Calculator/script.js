d6065c32caf3633192de2928fbb8bd36e118dc7a
RomaBeztelesnyi <mavric1234qwer@gmail.com>
2024-10-26 15:44:33 +0300

    General-actions

--- Calculator/script.js
+++ Calculator/script.js
@@ -4,7 +4,7 @@ let symbol = '';
 let finish = false;
 
 const digit = ['0','1','2','3','4','5','6','7','8','9'];
-const action = ['+', '-', 'X', '/'];
+const action = ['+', '-', 'x', '÷'];
 
 const out = document.querySelector('.calc-screen p');
 
@@ -15,3 +15,62 @@ function clearAll() {
     finish = '';
     out.textContent = 0;
 }
+
+document.querySelector('.ac').onclick = clearAll;
+
+document.querySelector('.buttons').addEventListener('click', (event) => {
+    if(!event.target.classList.contains('btn')) return;
+    if(event.target.classList.contains('ac')) return;
+
+    out.textContent = '';
+    const key = event.target.textContent;
+
+    if (digit.includes(key)) {
+        if (y ==='' && symbol === '') {
+            x += key;
+            out.textContent = x;
+        } else if (x !== '' && y !== '' && finish) {
+            y = key;
+            out.textContent = y;
+        }
+        else {
+            y += key;
+            out.textContent = y;
+        }
+        console.log(x, symbol, y)
+        return;
+        
+    }
+
+    if (action.includes(key)) { 
+        symbol = key;
+        out.textContent = symbol;
+        return;
+    }
+
+    if (key === '=') {
+        if(y === '') y = x;
+        switch (symbol) {
+            case '+':
+                x = (+x) + (+y);
+                break;
+            case '-':
+                x = x - y;
+                break;
+            case 'x':
+                x = x * y;
+                break;
+            case '÷':
+                if( y === '0') {
+                    alert("Error");
+                    clearAll();
+                    return;
+                }
+                x = x / y;
+                break;
+        }
+        console.log(x, symbol, y)
+        finish = true;
+        out.textContent = x;
+    }
+});
