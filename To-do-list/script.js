f81a5d573a65c6135743b0a58e8822ede6e15fb6
RomaBeztelesnyi <mavric1234qwer@gmail.com>
2025-09-10 13:50:02 +0300

    feat: added Js logic for edit task in to-do-list

--- To-do-list/script.js
+++ To-do-list/script.js
@@ -1,33 +1,63 @@
 const button = document.querySelector(".btn");
 const list = document.querySelector(".todo_list_item");
-const text = document.querySelector(".input-text");
+const inputText = document.querySelector(".input-text");
+
+let itemBeingEdited = null;
+let countList = 0;
 
 button.addEventListener("click", () => {
+  if (itemBeingEdited) {
+    // режим редагування
+    itemBeingEdited.firstChild.textContent = inputText.value;
+    itemBeingEdited = null;
+    button.textContent = "Add Task"; // повертаємо напис кнопки
+    inputText.value = "";
+    return;
+  }
   // Create Element
   const listItem = document.createElement("li");
   const groupBtn = document.createElement("div");
-  const removeBtn = document.createElement("button");
   const doneButton = document.createElement("button");
+  const editButton = document.createElement("button");
+  const removeBtn = document.createElement("button");
+  if (!inputText.value || inputText.value.trim() === "") {
+    alert("Prease enter task");
+    return;
+  } else {
+    listItem.innerText = inputText.value;
+  }
   // Add classes
   listItem.classList.add("list_item");
-  removeBtn.classList.add("btn-remove");
-  doneButton.classList.add("btn-done");
-  groupBtn.classList.add("btn-group");
+  listItem.setAttribute("id", `item-${countList++}`);
+  removeBtn.classList.add("btn_remove");
+  editButton.classList.add("btn_edit");
+  doneButton.classList.add("btn_done");
+  groupBtn.classList.add("btn_group");
 
-  // Content
-  listItem.innerText = text.value;
   removeBtn.innerText = "X";
+  editButton.innerText = "✎";
   doneButton.innerText = "✔";
-  groupBtn.append(doneButton, removeBtn);
+
+  groupBtn.append(doneButton, editButton, removeBtn);
   listItem.append(groupBtn);
   list.append(listItem);
-  text.value = "";
+
+  inputText.value = "";
+
   removeBtn.addEventListener("click", () => {
     listItem.remove(list);
-    removeBtn.remove(list);
-    doneButton.remove(list);
   });
+
+  editButton.addEventListener("click", editListItem);
+
   doneButton.addEventListener("click", () => {
     listItem.classList.toggle("done");
   });
 });
+
+function editListItem(e) {
+  const listItem = e.target.parentNode.parentNode;
+  inputText.value = listItem.firstChild.textContent;
+  itemBeingEdited = listItem;
+  button.textContent = "💾";
+}
--- To-do/script.js
+++ To-do/script.js
@@ -1,68 +1,72 @@
-const itemTemplate = document.getElementById('todo-list-item-template');
-const listElement = document.getElementById('todo-list');
-const editForm = document.getElementById('edit-form');
+const itemTemplate = document.getElementById("todo-list-item-template");
+const listElement = document.getElementById("todo-list");
+const editForm = document.getElementById("edit-form");
 
 let id = 0;
-function *makeId() {
-    while (true) {
-        yield id++
-    }
+function* makeId() {
+  while (true) {
+    yield id++;
+  }
 }
 
 const idGenerator = makeId();
 
 editForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const formData = new FormData(editForm);
-    const data = Object.fromEntries(formData.entries());
-    let listItem
+  event.preventDefault();
+  const formData = new FormData(editForm);
+  const data = Object.fromEntries(formData.entries());
+  let listItem;
 
-    if (editForm.dataset.mode === "create") {
-        if (data.title) {
-            listItem = itemTemplate.content.cloneNode(true);
-            listItem.querySelector('.todo-item').setAttribute('id', `item${id-idGenerator.next().value}`)
-            listItem.querySelector('.title').innerText = data.title;
-            listItem.querySelector('.desc').innerText = data.desc;
+  if (editForm.dataset.mode === "create") {
+    if (data.title) {
+      listItem = itemTemplate.content.cloneNode(true);
+      listItem
+        .querySelector(".todo-item")
+        .setAttribute("id", `item${id - idGenerator.next().value}`);
+      listItem.querySelector(".title").innerText = data.title;
+      listItem.querySelector(".desc").innerText = data.desc;
 
-            listElement.append(listItem);
-        }
-    } else if (editForm.dataset.mode === "edit") {
-        listItem = listElement.querySelector(`#${editForm.dataset.editId}`)
-        listItem.querySelector('.title').innerText = data.title;
-        listItem.querySelector('.desc').innerText = data.desc;
+      listElement.append(listItem);
     }
-    editForm.reset();
-    editForm.dataset.mode = "create";
-    editForm.dataset.editId = ""
+  } else if (editForm.dataset.mode === "edit") {
+    listItem = listElement.querySelector(`#${editForm.dataset.editId}`);
+    listItem.querySelector(".title").innerText = data.title;
+    listItem.querySelector(".desc").innerText = data.desc;
+  }
+  editForm.reset();
+  editForm.dataset.mode = "create";
+  editForm.dataset.editId = "";
 });
 
 function editItem(element) {
-    editForm.dataset.mode = "edit";
-    editForm.dataset.editId = element.id;
-    editForm.querySelector('[name=title]').value = element.querySelector('.title').innerText
-    editForm.querySelector('[name=desc]').value = element.querySelector('.desc').innerText
-
+  editForm.dataset.mode = "edit";
+  editForm.dataset.editId = element.id;
+  editForm.querySelector("[name=title]").value =
+    element.querySelector(".title").innerText;
+  editForm.querySelector("[name=desc]").value =
+    element.querySelector(".desc").innerText;
 }
-function toggleItemComplete(element)  {
-    element.classList.toggle("completed")
-}
-
-
-function deleteItem(element)  {
-    element.remove();
+function toggleItemComplete(element) {
+  element.classList.toggle("completed");
 }
 
+function deleteItem(element) {
+  element.remove();
+}
 
-listElement.addEventListener('click', (event) => {
-    const item = event.target.closest(".todo-item");
-    switch(event.target.dataset.action) {
-        case "edit":
-            editItem(item);
-        case "delete":
-            deleteItem(item);
-        case "complete":
-            toggleItemComplete(item);
-        default:
-            return;
-    }
+listElement.addEventListener("click", (event) => {
+  const item = event.target.closest(".todo-item");
+  switch (event.target.dataset.action) {
+    case "edit":
+      editItem(item);
+      break;
+    case "delete":
+      deleteItem(item);
+      break;
+    case "complete":
+      toggleItemComplete(item);
+      break;
+    default:
+      return;
+  }
 });
