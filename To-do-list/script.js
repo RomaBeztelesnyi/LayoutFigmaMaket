1e9049f0b795256e252d4312121278c6f27a48ac
RomaBeztelesnyi <mavric1234qwer@gmail.com>
2025-09-10 09:47:38 +0300

    refactor: changed CSS code

--- To-do-list/script.js
+++ To-do-list/script.js
@@ -1,31 +1,33 @@
-const button = document.querySelector('.btn');
-const list = document.querySelector('.todo_list_item');
-const text = document.querySelector('.input-text');
-
+const button = document.querySelector(".btn");
+const list = document.querySelector(".todo_list_item");
+const text = document.querySelector(".input-text");
 
 button.addEventListener("click", () => {
-    // Create Element
-    const listItem = document.createElement('li');
-    const removeBtn = document.createElement('button');
-    const doneButton = document.createElement('button');
-    // Add classes
-    listItem.classList.add('list_item');
-    removeBtn.classList.add('btn-remove');
-    doneButton.classList.add('btn-done');
-    
-    // Content
-    listItem.innerText = text.value;
-    removeBtn.innerText = 'X';
-    doneButton.innerText = '✔';
-    listItem.append(doneButton,removeBtn)
-    list.append(listItem)
-    text.value=''
-    removeBtn.addEventListener("click", () => {
-        listItem.remove(list);
-        removeBtn.remove(list);
-        doneButton.remove(list)
-    });
-    doneButton.addEventListener('click', () => {
-        listItem.classList.toggle('done');
-    });
+  // Create Element
+  const listItem = document.createElement("li");
+  const groupBtn = document.createElement("div");
+  const removeBtn = document.createElement("button");
+  const doneButton = document.createElement("button");
+  // Add classes
+  listItem.classList.add("list_item");
+  removeBtn.classList.add("btn-remove");
+  doneButton.classList.add("btn-done");
+  groupBtn.classList.add("btn-group");
+
+  // Content
+  listItem.innerText = text.value;
+  removeBtn.innerText = "X";
+  doneButton.innerText = "✔";
+  groupBtn.append(doneButton, removeBtn);
+  listItem.append(groupBtn);
+  list.append(listItem);
+  text.value = "";
+  removeBtn.addEventListener("click", () => {
+    listItem.remove(list);
+    removeBtn.remove(list);
+    doneButton.remove(list);
+  });
+  doneButton.addEventListener("click", () => {
+    listItem.classList.toggle("done");
+  });
 });
